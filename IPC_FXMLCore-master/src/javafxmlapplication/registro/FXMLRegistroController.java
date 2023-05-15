242eaf61d352f81e22f48e1fc9b4b5e170dad314
ErikUPV <119046735+ErikUPV@users.noreply.github.com>
2023-05-15 13:13:40 +0200

    Funcionamiento registro

--- IPC_FXMLCore-master/src/javafxmlapplication/registro/FXMLRegistroController.java
+++ IPC_FXMLCore-master/src/javafxmlapplication/registro/FXMLRegistroController.java
@@ -5,13 +5,22 @@
 package javafxmlapplication.registro;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.beans.Observable;
+import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.Effect;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -21,6 +30,7 @@ import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafxmlapplication.JavaFXMLApplication;
 import javafxmlapplication.Paginas;
+import model.*;
 
 /**
  * FXML Controller class
@@ -37,16 +47,50 @@ public class FXMLRegistroController implements Initializable {
     private ColumnConstraints columnaPrincipal;
     @FXML
     private ImageView perfilImageView;
+    @FXML
+    private TextField nickTextField;
+    @FXML
+    private TextField nameTextField;
+    @FXML
+    private TextField surnameTextField;
+    @FXML
+    private TextField tlfTextField;
+    @FXML
+    private PasswordField pwTextField;
+    @FXML
+    private PasswordField repeatPwTextField;
+    @FXML
+    private TextField creditCardTextField;
+    @FXML
+    private TextField cvcTextField;
+    @FXML
+    private Label badInputLabel;
+    @FXML
+    private Button registerButton;
+    
+    private Club club;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        // Listeners de los TextFields
+        try {
+            club = Club.getInstance();
+        } catch (ClubDAOException | IOException e) {
+            
+        }
+        
+        
+        
+        
+        
+        columnaPrincipal.minWidthProperty().set(400);
         // TODO
           borderPane.widthProperty().addListener(
                 (observable, oldV, newV) -> {
-                    if (newV.intValue() < 1000) columnaPrincipal.maxWidthProperty().bind(borderPane.widthProperty().multiply(0.35));
+                    if (newV.intValue() <= 800) columnaPrincipal.maxWidthProperty().bind(columnaPrincipal.minWidthProperty());
                     else columnaPrincipal.maxWidthProperty().bind(borderPane.widthProperty().multiply(0.3));
                 });
           
@@ -75,6 +119,70 @@ public class FXMLRegistroController implements Initializable {
             perfilImageView.setImage(new Image(imgPath.toString()));
         }
     }
+
+    @FXML
+    private void registerOnAction(ActionEvent event) {
+       registerButton.disableProperty().setValue(true);
+       if (nickTextField.textProperty().getValueSafe().isEmpty() || nickTextField.textProperty().getValueSafe().isBlank()) showErrorMessage(nickTextField);
+       else hideErrorMessage(nickTextField);
+       
+       if (nameTextField.textProperty().getValueSafe().isEmpty() || nameTextField.textProperty().getValueSafe().) showErrorMessage( nameTextField);
+       else hideErrorMessage(nameTextField);
+       
+       if (surnameTextField.textProperty().getValueSafe().isEmpty()) showErrorMessage( surnameTextField);
+       else hideErrorMessage(surnameTextField);
+       
+       if (tlfTextField.textProperty().getValueSafe().isEmpty()) showErrorMessage( tlfTextField);
+       else hideErrorMessage(tlfTextField);
+       
+       if (pwTextField.textProperty().getValueSafe().isEmpty()) showErrorMessage( pwTextField);
+       else hideErrorMessage(pwTextField);
+       
+       if (repeatPwTextField.textProperty().getValueSafe().isEmpty()) showErrorMessage( repeatPwTextField);
+       else hideErrorMessage(repeatPwTextField);
+       
+       if (creditCardTextField.textProperty().getValueSafe().isEmpty() || cvcTextField.textProperty().getValueSafe().isEmpty()){
+           showErrorMessage( creditCardTextField);
+           showErrorMessage( cvcTextField);
+
+       } else{
+           hideErrorMessage(creditCardTextField);
+           hideErrorMessage(cvcTextField);
+       }
+       registerButton.setDisable(false);
+       
+       
+       try {
+           club.registerMember(nameTextField.textProperty().getValue(),surnameTextField.textProperty().getValue(), tlfTextField.textProperty().getValue(),
+                   nickTextField.textProperty().getValue(), pwTextField.textProperty().getValue(), creditCardTextField.textProperty().getValue(), Integer.parseInt(cvcTextField.textProperty().getValue()), null);
+       } catch (ClubDAOException e) {
+           
+       }
+
+    }
    
+    private void showErrorMessage ( TextField field) {
+        String source = field.getId();
+        field.requestFocus();
+        badInputLabel.visibleProperty().setValue(true);
+        field.styleProperty().setValue("-fx-background-color: #FCE5E0;"
+                + "-fx-border-color: red;"
+                + "-fx-border-radius: 5 5 5 5;"
+                + "-fx-text-fill: red");
+        switch(source) {
+            case "nickTextField":
+                nickTextField.setPromptText("");
+        }
+       
+    }
     
-}
+     private void hideErrorMessage(TextField field){
+        
+        field.styleProperty().setValue(""); 
+    }
+
+    
+ 
+    }
+    
+
