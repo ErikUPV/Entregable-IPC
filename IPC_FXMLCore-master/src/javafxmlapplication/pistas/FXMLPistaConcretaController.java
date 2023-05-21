d5f2c426b85f0c8e7bf6e0eec3f7951244f7bf8f
ErikUPV <119046735+ErikUPV@users.noreply.github.com>
2023-05-21 11:10:43 +0200

    Movidas pista concreta
    WIP: Implementar reserva

--- IPC_FXMLCore-master/src/javafxmlapplication/pistas/FXMLPistaConcretaController.java
+++ IPC_FXMLCore-master/src/javafxmlapplication/pistas/FXMLPistaConcretaController.java
@@ -8,13 +8,22 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.WeekFields;
+import java.util.List;
+import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -22,6 +31,8 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ToolBar;
 import javafx.scene.image.Image;
@@ -42,6 +53,7 @@ public class FXMLPistaConcretaController implements Initializable {
 
     @FXML
     private ImageView imagenPista;
+    private Integer nPista;
     @FXML
     private Label title;
     @FXML
@@ -51,19 +63,38 @@ public class FXMLPistaConcretaController implements Initializable {
     @FXML
     private DatePicker datePicker;
     @FXML
-    private TableView<?> pistaTableView;
+    private TableView<CourtDayItem> pistaTableView;
     
     private Member member = null;
     
     private Club club;
     @FXML
     private ComboBox<?> comboBox;
+    @FXML
+    private Label dayLabel;
+    
+    private final String[] diaDeLaSemana = {"", "lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"};
+    
+    private final String[] mesDelAño = {"","enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
+    @FXML
+    private VBox dibujoVBox;
+    
+    private ObservableList<String> listaObs; 
+    @FXML
+    private TableColumn<CourtDayItem, String> horaCol;
+    @FXML
+    private TableColumn<CourtDayItem, String> estadoCol;
+    
+    private ObservableList<CourtDayItem> l;
+    
+    private List<Booking> list;
     /**
      * Initializes the controller class.
      */
   
     public void initPista(int pista) {
-        title.setText("Reservar Pista " + pista);
+        title.setText("Reservar pista " + pista);
+        
         File f;
         switch(pista) {
             case 1:
@@ -91,6 +122,20 @@ public class FXMLPistaConcretaController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        nPista = FXMLVerPistasController.getNumPista();
+        System.out.println("nPista: " + nPista);
+        borderPane.widthProperty().addListener( (observable, oldv, newv) -> {
+            if (newv.intValue() > 1200) {
+               
+                mainVBox.maxWidthProperty().bind(borderPane.widthProperty().multiply(0.6));
+                mainVBox.minWidthProperty().setValue(1200);
+            } else {
+                mainVBox.maxWidthProperty().bind(borderPane.widthProperty());
+                mainVBox.minWidthProperty().setValue(800);
+                
+            }
+        });
+        
         member = FXMLAutenticacionController.getMember();
         System.out.println("pista concreta: " + member.getNickName());
         try {
@@ -102,18 +147,59 @@ public class FXMLPistaConcretaController implements Initializable {
         
         comboBox.setPromptText(member.getName() + " " + member.getSurname());
         
-        pistaTableView.minHeightProperty().bind(mainVBox.heightProperty().multiply(0.67));
-        
-        borderPane.widthProperty().addListener( (observable, oldv, newv) -> {
-            if (newv.intValue() > 1200) {
-                mainVBox.maxWidthProperty().bind(borderPane.widthProperty().multiply(0.6));
-                mainVBox.minWidthProperty().setValue(1200);
+         borderPane.heightProperty().addListener( (observable, oldv, newv) -> {
+            if (newv.intValue() > 700) {
+                dibujoVBox.alignmentProperty().set(Pos.BASELINE_CENTER);
+            pistaTableView.minHeightProperty().bind(mainVBox.heightProperty().multiply(0.67));
+            
             } else {
-                mainVBox.maxWidthProperty().unbind();
-                mainVBox.minWidthProperty().setValue(800);
+                dibujoVBox.alignmentProperty().set(Pos.TOP_CENTER);
+                pistaTableView.minHeightProperty().unbind();
+                pistaTableView.setMinHeight(300);
             }
         });
         
+        
+//        borderPane.heightProperty().addListener((ob, oldv, newv)-> {
+//           if (newv.intValue() > 700) {
+//               
+//           } else {
+//               dibujoVBox.alignmentProperty().set(Pos.TOP_CENTER);
+//           }
+//        });
+
+
+        datePicker.valueProperty().setValue(LocalDate.now());
+
+        
+        
+        
+         horaCol.setCellValueFactory(cellData -> {
+            CourtDayItem item = cellData.getValue();
+            String hour = item.getFromTime().toString();
+            return new SimpleStringProperty(hour);
+        });
+
+        estadoCol.setCellValueFactory(cellData -> {
+            CourtDayItem item = cellData.getValue();
+            String status = item.getStatus() ? "Libre" : "Reservada";
+            return new SimpleStringProperty(status);
+        });
+         l = FXCollections.observableArrayList(
+                
+                
+        );
+         
+        
+       
+        
+       
+        
+        pistaTableView.setItems(l);
+        
+        datePicker.valueProperty().addListener((observable, oldValue, newValue) -> {
+            updateTableView(newValue);
+        });
         
         datePicker.setDayCellFactory((DatePicker picker) -> {
             return new DateCell() {
@@ -125,6 +211,13 @@ public class FXMLPistaConcretaController implements Initializable {
                 }
             };
         });
+        WeekFields weekFields = WeekFields.of(Locale.getDefault());
+        datePicker.valueProperty().addListener((ob, oldv, newv) -> {
+            int numDayNow = newv.get(weekFields.dayOfWeek());
+            int month = newv.getMonthValue();
+            dayLabel.textProperty().setValue("Pistas disponibles a " + diaDeLaSemana[numDayNow] + ", " + newv.getDayOfMonth() + " de " + mesDelAño[month] + " del " + newv.getYear());
+        });
+        
     }        
 
     @FXML
@@ -136,5 +229,62 @@ public class FXMLPistaConcretaController implements Initializable {
     }
     
     
+    private void updateTableView(LocalDate selectedDate) {
+        ObservableList<CourtDayItem> filteredData = FXCollections.observableArrayList();
+         for (int i = 0; i <= 12; i++) {
+            int b = 9;
+            filteredData.add(new CourtDayItem(datePicker.valueProperty().getValue(), LocalTime.of(b + i, 0), true));
+                
+        }
+        pistaTableView.setItems(filteredData);
+        checkAvaliability();
+        l.setAll(filteredData);
+    }
+
+    @FXML
+    private void reservarButtonOnAction(ActionEvent event) {
+        CourtDayItem aReservar = pistaTableView.getSelectionModel().getSelectedItem();
+        LocalDate dia = aReservar.getMadeForDay();
+        System.out.println("Dia: " + dia);
+        LocalTime time = aReservar.getFromTime();
+        System.out.println(dia + " " + time);
+        LocalDateTime diaYHora = dia.atTime(time);
+        System.out.println("LocalDateTime: " + dia + " " + time);
+        String creditCard = member.getCreditCard();
+        System.out.println("Credit card: " + creditCard);
+        boolean paid = false;
+        List<Court> courts = club.getCourts();
+        System.out.println("Num pista: " + nPista);
+        if (!creditCard.isEmpty()) paid = true;
+        Court court = club.getCourt("Pista " + nPista);
+       
+        System.out.println("Pista: " + court.getName());
+        try {
+            club.registerBooking(diaYHora, dia, time, paid, court, member);
+        } catch (ClubDAOException ex) {
+            Logger.getLogger(FXMLPistaConcretaController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        list = club.getCourtBookings("Pista " + nPista, dia);
+
+        
+         
+         pistaTableView.refresh();
+       
+    }
+    
+    
+    private void checkAvaliability() {
+         l.forEach(elementObs -> {
+             LocalDateTime t = elementObs.getMadeForDay().atTime(elementObs.getFromTime());
+            list.forEach(e -> {
+                if (t.equals(e.getBookingDate())) {
+                    elementObs.setStatus(CourtDayItem.OCUPADO);
+                }
+        });
+           
+        });
+    }
 
 }
--- IPC_FXMLCore-master/src/javafxmlapplication/pistas/FXMLVerPistasController.java
+++ IPC_FXMLCore-master/src/javafxmlapplication/pistas/FXMLVerPistasController.java
@@ -6,12 +6,17 @@ package javafxmlapplication.pistas;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafxmlapplication.JavaFXMLApplication;
 import javafxmlapplication.Paginas;
 import javafxmlapplication.autenticacion.FXMLAutenticacionController;
@@ -32,36 +37,66 @@ public class FXMLVerPistasController implements Initializable {
     
     
     
-    public void initMember(Member m) {
-        member = m;
-        
-       
+    protected static int numPista;
+    
+    public static int getNumPista() {
+        return numPista;
     }
+    @FXML
+    private Label pista1;
+    @FXML
+    private Label pista2;
+    @FXML
+    private Label pista3;
+    @FXML
+    private Label pista4;
+    @FXML
+    private Label pista5;
+    @FXML
+    private Label pista6;
+    
+    private Label[] pistas = {pista1,pista2,pista3,pista4,pista5,pista6};
+    private Club club;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         member = FXMLAutenticacionController.getMember();
         System.out.println("ver pistas: " + member.getNickName());
         System.out.println(member.toString());
+        
+        try {
+            club = Club.getInstance();
+            
+            
+        
+        } catch (ClubDAOException | IOException ex) {
+            Logger.getLogger(FXMLVerPistasController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
     }    
 
     @FXML
     private void pista1ButtonOnAction(ActionEvent event) throws IOException {
+        numPista = 1;
         FXMLLoader miCargador = JavaFXMLApplication.getLoader(Paginas.PISTA_CONCRETA);
             Parent root = miCargador.getRoot();
             if (root == null) root = miCargador.load();
             FXMLPistaConcretaController controlador = miCargador.getController();
             controlador.initPista(1);
+            
             JavaFXMLApplication.setRoot(root);
     }
 
     @FXML
     private void pista2ButtonOnAction(ActionEvent event) throws IOException {
+            numPista = 2;
             FXMLLoader miCargador = JavaFXMLApplication.getLoader(Paginas.PISTA_CONCRETA);
             Parent root = miCargador.getRoot();
             if (root == null) root = miCargador.load();
             FXMLPistaConcretaController controlador = miCargador.getController();
             controlador.initPista(2);
+            
             JavaFXMLApplication.setRoot(root);
     }
 
@@ -72,36 +107,43 @@ public class FXMLVerPistasController implements Initializable {
             if (root == null) root = miCargador.load();
             FXMLPistaConcretaController controlador = miCargador.getController();
             controlador.initPista(3);
+            numPista = 3;
             JavaFXMLApplication.setRoot(root);
     }
 
     @FXML
     private void pista4ButtonOnAction(ActionEvent event) throws IOException {
+            numPista = 4;
             FXMLLoader miCargador = JavaFXMLApplication.getLoader(Paginas.PISTA_CONCRETA);
             Parent root = miCargador.getRoot();
             if (root == null) root = miCargador.load();
             FXMLPistaConcretaController controlador = miCargador.getController();
             controlador.initPista(4);
+            
             JavaFXMLApplication.setRoot(root);
     }
 
     @FXML
     private void pista5ButtonOnAction(ActionEvent event) throws IOException {
+            numPista = 5;
             FXMLLoader miCargador = JavaFXMLApplication.getLoader(Paginas.PISTA_CONCRETA);
             Parent root = miCargador.getRoot();
             if (root == null) root = miCargador.load();
             FXMLPistaConcretaController controlador = miCargador.getController();
             controlador.initPista(5);
+            
             JavaFXMLApplication.setRoot(root);
     }
 
     @FXML
     private void pista6ButtonOnAction(ActionEvent event) throws IOException {
+            numPista = 6;
             FXMLLoader miCargador = JavaFXMLApplication.getLoader(Paginas.PISTA_CONCRETA);
             Parent root = miCargador.getRoot();
             if (root == null) root = miCargador.load();
             FXMLPistaConcretaController controlador = miCargador.getController();
             controlador.initPista(6);
+            
             JavaFXMLApplication.setRoot(root);
     }
     
